eda4270635eede1e69e95732485853006e3cb4fc
Marcio Silva de Souza <marcio_ssouza@yahoo.com.br>
2018-09-26 16:23:43 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/com/local/cursomc/CursomcApplication.java
+++ src/main/java/br/com/local/cursomc/CursomcApplication.java
@@ -63,6 +63,12 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
+		Categoria categoria3 = new Categoria(null, "Cama mesa e banho");
+		Categoria categoria4 = new Categoria(null, "Eletrônicos");
+		Categoria categoria5 = new Categoria(null, "Jardinagem");
+		Categoria categoria6 = new Categoria(null, "Decoração");
+		Categoria categoria7 = new Categoria(null, "Perfumaria");
+		
 		Produto produto1 = new Produto(null, "Computador", 2000.00);
 		Produto produto2 = new Produto(null, "Impressora" , 800.00);
 		Produto produto3 = new Produto(null, "Mouse", 80.00);
@@ -74,7 +80,7 @@ public class CursomcApplication implements CommandLineRunner{
 		produto2.getCategorias().addAll(Arrays.asList(categoria1,categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
 		
-		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2,categoria3,categoria4,categoria5,categoria6,categoria7));
 		produtoRepository.saveAll(Arrays.asList(produto1,produto2,produto3));
 		
 		Estado estado1 = new Estado(null,"Minas Gerais");
--- src/main/java/br/com/local/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/local/cursomc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.local.cursomc.domain.Categoria;
@@ -48,4 +51,8 @@ public class CategoriaService {
 			return categoriaRepository.findAll();
 		}
 		
+		public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+			PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction),orderBy);
+			return categoriaRepository.findAll(pageRequest);
+		}
 }
